42c63d07327b3339374fe7f95897865a43891455
srinivasan1609 <34212984+srinivasan1609@users.noreply.github.com>
2018-11-22 23:47:54 +0530

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {LoginComponent} from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'dashboard', component: DashboardComponent},
+                        {path:'login', component: LoginComponent},
+                        { path: '', redirectTo: '/login', pathMatch: 'full' }];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+
+  exports: [RouterModule],
+
+  imports: [ RouterModule.forRoot(routes)]
 })
 export class AppRoutingModule { }
